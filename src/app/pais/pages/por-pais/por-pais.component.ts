87cf6ab02b59797225dbd5d8725e1eff80a77b04
Adolfo Ajucum <martin_aj22@hotmail.com>
2023-03-17 16:22:09 -0600

    manejo de errores

--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -10,15 +10,19 @@ import { PaisService } from '../../services/pais.service';
 export class PorPaisComponent {
 
   termino: string = '';
+  hayError: boolean = false;
 
   constructor(private PaisService: PaisService) { }
 
   buscar(){
+    this.hayError = false;
     console.log(this.termino);
     
     this.PaisService.buscarPais( this.termino)
-    .subscribe( resp => {
+    .subscribe( (resp) => {
       console.log(resp);
+    }, (err) =>{
+      this.hayError = true;
     });
   }
 }

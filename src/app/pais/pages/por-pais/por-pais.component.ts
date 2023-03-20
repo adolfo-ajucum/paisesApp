7e77bd834760e07246de07217de2646b3eb0aed9
Adolfo Ajucum <martin_aj22@hotmail.com>
2023-03-20 15:32:53 -0600

    Llenar la tabla de paises

--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { PaisService } from '../../services/pais.service';
+import { Country } from '../../interfaces/pais.interface';
 
 @Component({
   selector: 'app-por-pais',
@@ -11,6 +12,7 @@ export class PorPaisComponent {
 
   termino: string = '';
   hayError: boolean = false;
+  paises: Country[] = [];
 
   constructor(private PaisService: PaisService) { }
 
@@ -20,7 +22,8 @@ export class PorPaisComponent {
     
     this.PaisService.buscarPais( this.termino)
     .subscribe( (resp) => {
-      console.log(resp);
+      this.paises = resp;
+      console.log(this.paises);
     }, (err) =>{
       this.hayError = true;
     });
--- src/app/pais/pais.module.ts
+++ src/app/pais/pais.module.ts
@@ -5,6 +5,7 @@ import { PorPaisComponent } from './pages/por-pais/por-pais.component';
 import { VerPaisComponent } from './pages/ver-pais/ver-pais.component';
 import { PorRegionComponent } from './pages/por-region/por-region.component';
 import { FormsModule } from '@angular/forms';
+import { RouterLink, RouterModule } from '@angular/router';
 
 
 
@@ -23,7 +24,8 @@ import { FormsModule } from '@angular/forms';
   ],
   imports: [ 
     CommonModule,
-    FormsModule
+    FormsModule,
+    RouterModule
   ]
 })
 export class PaisModule { }

c7425e6ee16732e0420b6daa95cdf4d2057e3c9d
adolfo-ajucum <martin_aj22@hotmail.com>
2023-03-27 16:13:27 -0600

    por capital

--- src/app/pais/pages/por-capital/por-capital.component.ts
+++ src/app/pais/pages/por-capital/por-capital.component.ts
@@ -1,16 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { PaisService } from '../../services/pais.service';
+import { Country } from '../../interfaces/pais.interface';
 
 @Component({
-  selector: 'app-por-capital',
+  selector: 'app-por-pais',
   templateUrl: './por-capital.component.html',
   styles: [
   ]
 })
-export class PorCapitalComponent implements OnInit {
+export class PorCapitalComponent {
 
-  constructor() { }
+  termino: string = '';
+  hayError: boolean = false;
+  paises: Country[] = [];
 
-  ngOnInit(): void {
+  constructor(private PaisService: PaisService) { }
+
+  buscar(termino: string){
+    this.hayError = false;
+    console.log(this.termino);
+    
+    this.PaisService.buscarCapital( termino)
+    .subscribe( (resp) => {
+      this.paises = resp;
+      console.log(this.paises);
+    }, (err) =>{
+      this.hayError = true;
+    });
   }
-
 }
